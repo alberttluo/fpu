4125c92d708596cb752900dd02e26e9428142ead
Albert Luo <albertlu@hh-1305-06.lan.local.cmu.edu>
2025-11-23 22:07:03 -0500

    test: Added different display tasks for add and sub

--- src/fpuadd_test.sv
+++ src/fpuadd_test.sv
@@ -20,8 +20,8 @@ module fpuadd_test();
 
   FPU DUT(.*);
 
-  initial begin
-    $monitor("fpuIn1(%b)    fpuIn2(%b)\n",
+  task displayAddInfo();
+    $display("fpuIn1(%b)    fpuIn2(%b)\n",
              fpuIn1, fpuIn2,
              "S1(%b)E1(%b)Sig1(%b)    S2(%b)E2(%b)Sig2(%b)\n",
              DUT.fpuAdder.fpuAddSubS1, DUT.fpuAdder.fpuAddSubE1, DUT.fpuAdder.fpuAddSubSig1,
@@ -33,28 +33,58 @@ module fpuadd_test();
              "ZCNV(%b)\n",
              condCodes,
              "shiftIn1(%b), effS2(%b), adjExp(%b), adjSig(%b), nonAdjSig(%b), extSigOut(%b)\n",
-             addSubView.shiftIn1, addSubView.effS2, addSubView.adjExp, addSubView.adjSig, 
-             addSubView.nonAdjSig, addSubView.extSigOut,
+             DUT.fpuAdder.shiftIn1, DUT.fpuAdder.effS2, DUT.fpuAdder.adjExp, DUT.fpuAdder.adjSig, 
+             DUT.fpuAdder.nonAdjSig, DUT.fpuAdder.extSigOut,
              "=====================================================\n");
+    #10;
+  endtask
 
+  task displaySubInfo();
+    $display("fpuIn1(%b)    fpuIn2(%b)\n",
+             fpuIn1, fpuIn2,
+             "S1(%b)E1(%b)Sig1(%b)    S2(%b)E2(%b)Sig2(%b)\n",
+             DUT.fpuSubtracter.fpuAddSubS1, DUT.fpuSubtracter.fpuAddSubE1, DUT.fpuSubtracter.fpuAddSubSig1,
+             DUT.fpuSubtracter.fpuAddSubS2, DUT.fpuSubtracter.fpuAddSubE2, DUT.fpuSubtracter.fpuAddSubSig2,
+             "fpuOP(%s)\n",
+             op.name,
+             "fpuOut(%b)\n",
+             fpuOut,
+             "ZCNV(%b)\n",
+             condCodes,
+             "shiftIn1(%b), effS2(%b), adjExp(%b), adjSig(%b), nonAdjSig(%b), extSigOut(%b)\n",
+             DUT.fpuSubtracter.shiftIn1, DUT.fpuSubtracter.effS2, DUT.fpuSubtracter.adjExp, DUT.fpuSubtracter.adjSig, 
+             DUT.fpuSubtracter.nonAdjSig, DUT.fpuSubtracter.extSigOut,
+             "=====================================================\n");
+    #10;
+  endtask
 
+  initial begin
+    $display("Testing 1 + 0...");
     fpuIn1 <= 16'h3C00;
     fpuIn2 <= '0;
     op <= FPU_ADD;
 
     #10;
 
-    fpuIn1 <= 16'h3C00;
+    displayAddInfo();
+
+    $display("Testing 2 + 1...");
+    fpuIn1 <= 16'h4000;
     fpuIn2 <= 16'h3C00;
-    op <= FPU_SUB;
+    op <= FPU_ADD;
 
     #10;
 
+    displayAddInfo();
+
+    $display("Testing 1 - 0...");
     fpuIn1 <= 16'h3C00;
     fpuIn2 <= 16'h0000;
     op <= FPU_SUB;
 
     #10;
+
+    displaySubInfo();
     $finish;
   end
 endmodule : fpuadd_test
