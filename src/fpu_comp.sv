edc73d1630b29ad6a3d68e85125f29a12aec92c9
Albert Luo <albertlu@hh-1305-01.lan.local.cmu.edu>
2025-11-30 13:14:29 -0500

    feat: Add comparisons for infs

--- src/fpu_comp.sv
+++ src/fpu_comp.sv
@@ -6,6 +6,8 @@
 
 module fpuComp16
   (input  fp16_t fpuIn1, fpuIn2,
+   input  logic  isInf1, isInf2,
+   input  logic  isNaN1, isNaN2,
    output logic  lt, eq, gt);
 
   logic [14:0] mag1;
@@ -13,18 +15,28 @@ module fpuComp16
   logic magLt;
   logic magGt;
 
+  logic noNaNs, noInfs;
+  assign noNaNs = (~isNaN1 & ~isNaN2);
+  assign noInfs = (~isInf1 & ~isInf2);
+
   assign mag1 = {fpuIn1.exp, fpuIn1.frac};
   assign mag2 = {fpuIn2.exp, fpuIn2.frac};
   assign magLt = (mag1 < mag2);
   assign magGt = (mag1 > mag2);
 
-  assign lt = (fpuIn1.sign & fpuIn2.sign & magGt) |
-              (~fpuIn1.sign & ~fpuIn2.sign & magLt) |
-              (fpuIn1.sign & ~fpuIn2.sign);
+  assign lt = ((fpuIn1.sign & fpuIn2.sign & magGt) |
+               (~fpuIn1.sign & ~fpuIn2.sign & magLt) |
+               (fpuIn1.sign & ~fpuIn2.sign) |
+               // Less than always true if fpuIn1 = -inf, unless fpuIn2 = -inf
+               // as well, in which case less than is false.
+               (isInf1 & fpuIn1.sign)) & noNaNs & ~(isInf2 & fpuIn2.sign);
 
-  assign eq = (fpuIn1 == fpuIn2);
+  assign eq = ((fpuIn1 == fpuIn2) | (isInf1 & isInf2 & fpuIn1.sign == fpuIn2.sign)) & noNaNs;
 
-  assign gt = (fpuIn1.sign & fpuIn2.sign & magLt) |
-              (~fpuIn1.sign & ~fpuIn2.sign & magGt) |
-              (~fpuIn1.sign & fpuIn2.sign);
+  assign gt = ((fpuIn1.sign & fpuIn2.sign & magLt) |
+               (~fpuIn1.sign & ~fpuIn2.sign & magGt) |
+               (~fpuIn1.sign & fpuIn2.sign) |
+               // Greater than always true if fpuIn1 = inf, unless fpuIn2 = inf
+               // as well, in which case greater than is false.
+               (isInf1 & ~fpuIn1.sign)) & noNaNs & ~(isInf2 & ~fpuIn2.sign);
 endmodule : fpuComp16
