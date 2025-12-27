dc7ac16121dd6627566873ec879cdf34abd00c78
Albert Luo <albertlu@ece000.ece.local.cmu.edu>
2025-12-26 21:52:37 -0500

    feat: Division module works (floored integer)

--- src/divider.sv
+++ src/divider.sv
@@ -16,18 +16,94 @@ typedef enum logic [1:0] {
   DIV_DONE
 } fpuDivState_t;
 
+// Computes divIn1 / divIn2
 module fpuDivider
   #(parameter int WIDTH = 16)
   (input  logic [WIDTH - 1:0] divIn1, divIn2,
    input  logic               start, clock, reset,
    output logic [WIDTH - 1:0] divOut,
    output logic               done);
+
+  // Counts number of iterations.
+  logic [$clog2(WIDTH + 1) - 1:0] shiftCounter;
+
+  // Top WIDTH bits store the (partial) remainder, bottom WIDTH bits store the current quotient.
+  logic [2 * WIDTH - 1:0] remShiftReg;
+  logic [2 * WIDTH - 1:0] shiftedAQ;
+  logic [WIDTH - 1:0] nextPartialRem;
+  logic [WIDTH - 1:0] nextDivOut;
+  logic [WIDTH:0] tempRemReg;
+  logic [WIDTH - 1:0] latchedIn1;
+  logic [WIDTH - 1:0] latchedIn2;
+
+  logic compEn, compDone;
+
+  logic restore;
+
+  always_ff @(posedge clock, posedge reset) begin
+    if (start) begin
+      shiftCounter <= WIDTH;
+      remShiftReg <= {WIDTH'(0), divIn1};
+      latchedIn1 <= divIn1;
+      latchedIn2 <= divIn2;
+    end
+
+    else if (compEn) begin
+      shiftCounter <= shiftCounter - 1;
+      remShiftReg <= {nextPartialRem, nextDivOut};
+    end
+  end
+
+  assign shiftedAQ = remShiftReg << 1;
+
+  // Remainder - divisor.
+  assign tempRemReg = (shiftedAQ[2 * WIDTH - 1:WIDTH] - latchedIn2);
+
+  // Check if subtraction overflowed (must restore).
+  assign restore = (tempRemReg[WIDTH]);
+
+  assign nextPartialRem = (restore) ? (shiftedAQ[2 * WIDTH - 1:WIDTH]) : tempRemReg;
+
+  assign divOut = remShiftReg[WIDTH - 1:0];
+
+  // Append 1 bit to quotient if no overflow, otherwise restore.
+  assign nextDivOut = {shiftedAQ[WIDTH - 1:1] , ~restore};
+
+  // Division is done when all dividend bits shifted.
+  assign compDone = (shiftCounter == 1);
+
+  fpuDividerFSM FSM(.*);
+
 endmodule : fpuDivider
 
 module fpuDividerFSM
   (input  logic clock, reset, compDone, start,
-   output logic compEn,
-   output logic done);
+   output logic compEn, done);
+
+  fpuDivState_t currState, nextState;
+
+  always_ff @(posedge clock, posedge reset) begin
+    if (reset) begin
+      currState <= DIV_WAIT;
+    end
+
+    else begin
+      currState <= nextState;
+    end
+  end
+
+  always_comb begin
+    unique case (currState)
+      DIV_WAIT: nextState = (start) ? DIV_COMP : DIV_WAIT;
+
+      DIV_COMP: nextState = (compDone) ? DIV_DONE : DIV_COMP;
+
+      DIV_DONE: nextState = DIV_DONE;
+    endcase
+  end
+
+  assign compEn = (currState == DIV_COMP);
+  assign done = (currState == DIV_DONE);
 endmodule : fpuDividerFSM
 
 `endif
---
+++ test/divider_test.sv
@@ -0,0 +1,66 @@
+/*
+* divider_test.sv: Testbench for restoring (integer) division.
+*
+* Author: Albert Luo (albertlu)
+*/
+
+`include "divider.sv"
+
+module divider_test();
+  localparam int WIDTH = 8;
+
+  logic [WIDTH - 1:0] divIn1;
+  logic [WIDTH - 1:0] divIn2;
+  logic start, clock, reset;
+  logic [WIDTH - 1:0] divOut;
+  logic done;
+
+  fpuDivider #(.WIDTH(WIDTH)) DUT(.*);
+
+  task automatic doDiv
+    (input logic [WIDTH - 1:0] in1,
+     input logic [WIDTH - 1:0] in2);
+
+    reset <= 0;
+    #1;
+    reset <= 1;
+    #1;
+    reset <= 0;
+
+    start <= 1;
+    divIn1 <= in1;
+    divIn2 <= in2;
+    @(posedge clock);
+    start <= 0;
+
+    while (~done) @(posedge clock);
+
+    $display("Result of %d / %d = %d.",
+             divIn1, divIn2, divOut);
+    assert(divOut == divIn1 / divIn2) else $display("Wrong answer.");
+  endtask
+
+  function automatic logic [WIDTH - 1:0] randNum();
+    return WIDTH'($urandom);
+  endfunction
+
+  initial begin
+    reset = 1;
+    reset <= 0;
+    clock = 0;
+
+    forever #10 clock = ~clock;
+  end
+
+  initial begin
+    // $monitor("divIn1(%d) divIn2(%d) divOut(%b)\n",
+    //          divIn1, divIn2, divOut,
+    //          "currState = %s, nextState = %s\n",
+    //          DUT.FSM.currState, DUT.FSM.nextState);
+    for (int i = 0; i < 100; i++) begin
+      doDiv(randNum(), randNum());
+    end
+
+    $finish;
+  end
+endmodule : divider_test
